d9d029f81bdb841a899c507e3727b0fdc56ea1fa
cn3085 <cn3085@gmail.com>
2022-04-07 15:24:56 +0900

    add meta tag

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,18 @@ export default function Home() {
   return (
     <React.StrictMode>
       <Head>
+        <meta charSet="UTF-8"></meta>
+        <meta name="description" content="가나다라설명"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta property="og:title" content="카카오 제목"></meta>
+        <meta property="og:description" content="내용 요약"></meta>
+        <meta
+          property="og:image"
+          content="http://anniversary-chi.vercel.app/images/aws-logo.png"
+        ></meta>
         <title>hello</title>
         <style>
           @import
